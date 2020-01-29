b9fa60f2ff13aac9dd7aad86cbfa20f15af84068
akash.kini <akash.kini@itiviti.com>
2020-01-29 10:49:58 +0000

    Add support to run the script for various admin systems

--- check_tblog.py
+++ check_tblog.py
@@ -3,10 +3,10 @@ import subprocess
 import os
 
 doc = xml.dom.minidom.parse("configuration.xml")
-STDERR_FILE = open("STDERR_FILE", "w")
+STDERR_FILE = open("/opt/tbricks/logs/component_logs/STDERR_FILE", "w")
 
 original_admin_center = os.environ["TBRICKS_ADMIN_CENTER"]
-
+original_path = os.environ["PATH"]
 #os.environ["TBRICKS_ADMIN_CENTER"] = config["admin_system"]
 LOG_DIRECTORY = "/opt/tbricks/logs/component_logs"
 
@@ -48,49 +48,65 @@ def append_all_error_warn(generic_error_warn, specific_error_warn):
     return term
 
 
-# loop through all <component> tags
-for component_type in doc.getElementsByTagName("component"):  
-    # check if component type is ALL to get generic error/warning                        
-    if (component_type.getAttribute("type") == "ALL"):  
-        skip, search = get_all_error_warn(component_type)
-        generic_error_warn_to_skip = "|".join(skip)
-        generic_error_warn_to_search = "|".join(search)
-    else:
-        skip, search = get_all_error_warn(component_type)
-        specific_error_warn_to_skip = "|".join(skip)
-        specific_error_warn_to_search = "|".join(search)
+for admin_system in doc.getElementsByTagName("admin_system"):  
+    # set admin system
+    current_admin_system = admin_system.getAttribute("name")
+    os.environ["TBRICKS_ADMIN_CENTER"] = current_admin_system
 
-        component_name_list.extend(subprocess.check_output("tbcomponent get type  | egrep -B1 '"+component_type.getAttribute("type")+"' | egrep -v '"+component_type.getAttribute("type")+"|--' | awk '{print $3}'", shell="True").split("\n"))
-        component_name_list.remove("")
+    # get and set PATH to tbricks bin/ directory
+    old_path = os.environ["PATH"]
+    new_path = admin_system.getAttribute("path")+":"+old_path
+    print("Admin system: "+current_admin_system+"\nPath: "+new_path)
+    
+    print("\n"+"-"*70+" Checking Admin System: "+current_admin_system+" "+"-"*70)
+    # loop through all <component> tags
+    for component_type in doc.getElementsByTagName("component"):  
+        # check if component type is ALL to get generic error/warning                        
+        if (component_type.getAttribute("type") == "ALL"):  
+            skip, search = get_all_error_warn(component_type)
+            generic_error_warn_to_skip = "|".join(skip)
+            generic_error_warn_to_search = "|".join(search)
+        else:
+            skip, search = get_all_error_warn(component_type)
+            specific_error_warn_to_skip = "|".join(skip)
+            specific_error_warn_to_search = "|".join(search)
 
-        # loop through all individual components of type <component>
-        for component in component_name_list:
-            # append all skip terms
-            skip = append_all_error_warn(generic_error_warn_to_skip, specific_error_warn_to_skip)
-            # append all skip terms
-            search = append_all_error_warn(generic_error_warn_to_search, specific_error_warn_to_search)
+            component_name_list.extend(subprocess.check_output(admin_system.getAttribute("path")+"tbcomponent get type  | egrep -B1 '"+component_type.getAttribute("type")+"' | egrep -v '"+component_type.getAttribute("type")+"|--' | awk '{print $3}'", shell="True").split("\n"))
+            component_name_list.remove("")
 
-            print("\n"+"-"*70+" Checking component "+component+" "+"-"*70)
-            try:
-                if(skip != ""):
-                    #error_warn_to_skip = config["component_details"][component_type_list.index(component_type)]["specific_error_warning_to_skip"] + "|" +config["common_error_warning_to_skip"]
-                    data_to_write = subprocess.check_output("tblog "+component+" -n | tbgrep -v '"+skip+"'", shell="True")
-                else:
-                    data_to_write = subprocess.check_output("tblog "+component+" -n ", shell="True")
-                if data_to_write:
-                    OUTPUT_FILE = open(LOG_DIRECTORY+"/"+component+"_check.log", "w+")
-                    generate_alert_for_components.append(component)
-                    OUTPUT_FILE.write(data_to_write)
-                    OUTPUT_FILE.close()
-            except subprocess.CalledProcessError as exc:
-                STDERR_FILE.write("\nFor Component: "+component+"\nReturn Code: "+str(exc.returncode)+"\nOutput:"+str(exc.output))
-            component_name_list = []
-            skip = ""
-            search = ""
+            # loop through all individual components of type <component>
+            for component in component_name_list:
+                # append all skip terms
+                skip = append_all_error_warn(generic_error_warn_to_skip, specific_error_warn_to_skip)
+                # append all skip terms
+                search = append_all_error_warn(generic_error_warn_to_search, specific_error_warn_to_search)
+
+                print("\n"+"-"*70+" Checking component "+component+" "+"-"*70)
+                try:
+                    if(skip != ""):
+                        #error_warn_to_skip = config["component_details"][component_type_list.index(component_type)]["specific_error_warning_to_skip"] + "|" +config["common_error_warning_to_skip"]
+                        data_to_write = subprocess.check_output(admin_system.getAttribute("path")+"tblog "+component+" -n | tbgrep -v '"+skip+"'", shell="True")
+                    else:
+                        data_to_write = subprocess.check_output(admin_system.getAttribute("path")+"tblog "+component+" -n ", shell="True")
+                    if data_to_write:
+                        OUTPUT_FILE = open(LOG_DIRECTORY+"/"+component+"_check.log", "a+")
+                        generate_alert_for_components.append(component)
+                        OUTPUT_FILE.write(data_to_write)
+                        OUTPUT_FILE.close()
+                except subprocess.CalledProcessError as exc:
+                    STDERR_FILE.write("\nFor Component: "+component+" of Admin System:"+current_admin_system+"\nReturn Code: "+str(exc.returncode)+"\nOutput:"+str(exc.output))
+                component_name_list = []
+                skip = ""
+                search = ""
 
 STDERR_FILE.close()
-os.environ["TBRICKS_ADMIN_CENTER"] = original_admin_center
 
+# reset original admin system and path
+os.environ["TBRICKS_ADMIN_CENTER"] = original_admin_center
+os.environ["PATH"] = original_path
+
+print("\n\nAll logs saved in directory: /opt/tbricks/logs/component_logs/")
+print("\nSTD ERR copied to file: /opt/tbricks/logs/component_logs/STDERR_FILE")
 print("\n\nAlerts to be sent for following components:")
 
 os.environ["TBRICKS_ADMIN_CENTER"] = original_admin_center
